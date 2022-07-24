3a0c65c13adbcece6bf24a62b9f1efc1941982e4
Dibyendu Laha <90048704+Laha-Dibyendu@users.noreply.github.com>
2022-07-24 20:48:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,5 @@ st.set_page_config(page_title=string, page_icon="🔍")
 st.title('Sum two numbers')
 x = st.number_input('Enter a number')
 y = st.number_input('Enter another number')
-# if (x).is_integer() :
-#     if x%2==0:st.write("your number is even")
-#     elif x%2!=0:st.write("your number is odd")
-# else:
-#st.write("The sum of the two given numbers is :", x+y)
-p=x+y
-if (p).is_integer():
-    st.write("The sum of the two given numbers is :", p)
-else:
-    st.write("The sum of the two given numbers is :"+round(p,2)+" (rounded off upto 2 decimals)")
+
+st.write("The sum of the two given numbers is :", x+y)
