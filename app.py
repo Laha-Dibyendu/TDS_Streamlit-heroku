600c5f95abac0b8cb20c38ad953f2e41f5e99351
Dibyendu Laha <90048704+Laha-Dibyendu@users.noreply.github.com>
2022-07-24 20:08:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+string = "Sum of Two Numbers"
+st.set_page_config(page_title=string, page_icon="🔍")
+
+st.title('Sum two numbers')
+x = st.number_input('Enter a number')
+y = st.number_input('Enter another number')
+# if (x).is_integer() :
+#     if x%2==0:st.write("your number is even")
+#     elif x%2!=0:st.write("your number is odd")
+# else:
+st.write("The sum of the two given numbers is :", x+y)
